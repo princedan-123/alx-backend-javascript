51a257822de8f8ec7c094344f66cc97b6ccb4039
Daniel Ebuka Mabia <danielebukamabia@gmail.com>
2024-03-06 12:06:41 +0000

    task 3

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -2,7 +2,7 @@ import { uploadPhoto, createUser } from './utils';
 
 export default function handleProfileSignup() {
   const response = Promise.all([uploadPhoto(), createUser()]);
-  response.then((response) => {
+  return response.then((response) => {
     console.log(response[0].body, response[1].firstName, response[1].lastName);
-  });
+  }).catch(() => { console.log('Signup system offline'); });
 }
