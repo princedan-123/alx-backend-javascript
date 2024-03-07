8513a82a1e03e4ac1afb688c0ae54527e082444c
Daniel Ebuka Mabia <danielebukamabia@gmail.com>
2024-03-07 11:47:28 +0000

    task 1

---
+++ 0x02-ES6_classes/1-make_classrooms.js
@@ -0,0 +1,7 @@
+/* modifying the ClassRoom class */
+
+import ClassRoom from './0-classroom';
+
+export default function initializeRooms() {
+  return [new ClassRoom(19), new ClassRoom(20), new ClassRoom(34)];
+}
