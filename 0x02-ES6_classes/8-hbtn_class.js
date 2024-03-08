c4d8adc5dab756391258de214a213f87b1b43345
Daniel Ebuka Mabia <danielebukamabia@gmail.com>
2024-03-08 04:36:40 +0000

    task 8

---
+++ 0x02-ES6_classes/8-hbtn_class.js
@@ -0,0 +1,16 @@
+export default class HolbertonClass {
+  constructor(size, location) {
+    if (typeof size === 'number' && typeof location === 'string') {
+      this._size = size;
+      this._location = location;
+    }
+  }
+
+  valueOf() {
+    return this._size;
+  }
+
+  toString() {
+    return this._location;
+  }
+}
