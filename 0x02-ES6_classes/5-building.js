a0ab210a76344656d040aa3f25e250df6c5346f1
Daniel Ebuka Mabia <danielebukamabia@gmail.com>
2024-03-08 16:34:19 +0000

    task 5

--- 0x02-ES6_classes/5-building.js
+++ 0x02-ES6_classes/5-building.js
@@ -3,16 +3,18 @@ export default class Building {
     if (typeof sqft === 'number') {
       this._sqft = sqft;
     }
+    if (this.constructor.name !== 'Building' && typeof this.evacuationWarningMessage !== 'function') {
+      throw new Error('Class extending Building must override evacuationWarningMessage');
+    }
   }
+
   get sqft() {
     return this._sqft;
   }
+
   set sqft(newSqft) {
     if (typeof newSqft === 'number') {
       this._sqft = newSqft;
     }
-  }
-  evacuationWarningMessage() {
-     throw new Error('Class extending Building must override evacuationWarningMessage')
   }
 }
