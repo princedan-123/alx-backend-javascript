ec90550fa05cbeef32f6712b871fbf1bf38cac5c
Daniel Ebuka Mabia <danielebukamabia@gmail.com>
2024-03-12 15:42:07 +0000

    task 9

---
+++ 0x03-ES6_data_manipulation/9-groceries_list.js
@@ -0,0 +1,9 @@
+/* A function that creates a map object */
+
+function groceriesList() {
+  const groceries = new Map(
+    [['Apples', 10], ['Tomatoes', 10], ['Pasta', 1], ['Rice', 1], ['Banana', 5]],
+  );
+  return groceries;
+}
+export default groceriesList;
