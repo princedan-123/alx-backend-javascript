6752b55340ef028494392661eb25105fc7398982
Daniel Ebuka Mabia <danielebukamabia@gmail.com>
2024-03-12 11:58:35 +0000

    task 5

---
+++ 0x03-ES6_data_manipulation/5-typed_arrays.js
@@ -0,0 +1,18 @@
+/* A function that creates an ArrayBuffer */
+
+function createInt8TypedArray(length, position, value) {
+  if (!(typeof length === 'number' && typeof position === 'number')) {
+    throw new Error('Position outside range');
+  }
+  if (!(typeof value === 'number')) {
+    throw new Error('Position outside range');
+  }
+  if (position > length) {
+    throw new Error('Position outside range');
+  }
+  const buffer = new ArrayBuffer(length);
+  const view = new DataView(buffer);
+  view.setInt8(position, value);
+  return buffer;
+}
+export default createInt8TypedArray;
