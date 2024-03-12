8e50d415b6310672ed285b92d9f6595cda670457
Daniel Ebuka Mabia <danielebukamabia@gmail.com>
2024-03-12 14:32:38 +0000

    task 8

---
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -0,0 +1,16 @@
+function cleanSet(set, startString) {
+  const { length } = startString;
+  const array = [];
+  let newString = '';
+  if (length > 0) {
+    for (const item of set) {
+      if (item.startsWith(startString)) {
+        const newString = item.slice(length, -1);
+        array.push(newString);
+      }
+    }
+    newString = array.join('-');
+  }
+  return newString;
+}
+export default cleanSet;
