498532806e118fb37bb2032549c18991d5773e44
Daniel Ebuka Mabia <danielebukamabia@gmail.com>
2024-03-12 12:01:00 +0000

    task 5

--- 0x03-ES6_data_manipulation/5-typed_arrays.js
+++ 0x03-ES6_data_manipulation/5-typed_arrays.js
@@ -13,6 +13,6 @@ function createInt8TypedArray(length, position, value) {
   const buffer = new ArrayBuffer(length);
   const view = new DataView(buffer);
   view.setInt8(position, value);
-  return view
+  return view;
 }
 export default createInt8TypedArray;
