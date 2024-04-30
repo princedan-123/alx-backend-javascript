76d536fcdbea8764a04f6fa1d723ef2e64309ea7
Daniel Ebuka Mabia <danielebukamabia@gmail.com>
2024-04-30 07:52:03 +0000

    used setHeader

--- 0x05-Node_JS_basic/4-http.js
+++ 0x05-Node_JS_basic/4-http.js
@@ -3,7 +3,8 @@
 const http = require('http');
 
 const app = http.createServer((request, response) => {
-  response.writeHead(200, { 'Content-Type': 'text/plain' });
+  response.statusCode = 200;
+  response.setHeader('Content-Type', 'text/plain');
   response.end('Hello Holberton School!');
 });
 app.listen(1245);
