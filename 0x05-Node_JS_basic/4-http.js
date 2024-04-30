ca6305e62a39882c7ffea246974b916ff4f0e513
Daniel Ebuka Mabia <danielebukamabia@gmail.com>
2024-04-30 05:10:23 +0000

    task 4

--- 0x05-Node_JS_basic/4-http.js
+++ 0x05-Node_JS_basic/4-http.js
@@ -2,9 +2,9 @@
 
 const http = require('http');
 
-const app = http.createServer((resquest, response) => {
+const app = http.createServer((request, response) => {
   response.writeHead(200, { 'Content-Type': 'text/plain' });
-  response.end('Hello Holberton School!', 'utf-8');
+  response.end('Hello Holberton School!');
 });
 app.listen(1245);
 
