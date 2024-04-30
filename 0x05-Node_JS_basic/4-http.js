94efd2fa03aefeffa209c760ece83cf1dbfa7bbb
Daniel Ebuka Mabia <danielebukamabia@gmail.com>
2024-04-30 05:05:38 +0000

    added header

--- 0x05-Node_JS_basic/4-http.js
+++ 0x05-Node_JS_basic/4-http.js
@@ -3,6 +3,7 @@
 const http = require('http');
 
 const app = http.createServer((resquest, response) => {
+  response.writeHead(200, { 'Content-Type': 'text/plain' });
   response.end('Hello Holberton School!', 'utf-8');
 });
 app.listen(1245);
