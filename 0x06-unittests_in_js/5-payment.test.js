48ea953a9986dea19403e0df4dcd8dc8a7325534
Daniel Ebuka Mabia <danielebukamabia@gmail.com>
2024-05-03 18:47:17 +0000

    updated test

--- 0x06-unittests_in_js/5-payment.test.js
+++ 0x06-unittests_in_js/5-payment.test.js
@@ -10,8 +10,10 @@ describe('testing the use of hooks', () => {
 		expect(sendPaymentRequestToApi(100, 20)).to.equal(120);
 		expect(consolespy.calledWithExactly('The total is: 120')).to.be.true;
 		sinon.assert.calledOnce(consolespy);
-		consolespy.restore();
 	});
+	afterEach(() => {
+		consolespy.restore()
+	})
 	it('testing sendPaymentRequestToApi with with 10, and 10', () => {
 		expect(sendPaymentRequestToApi(10, 10)).to.equal(20);
 		expect(consolespy.calledWithExactly('The total is: 20')).to.be.true;
