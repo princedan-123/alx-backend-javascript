a35156d1825f47fed71b5f0488f9b7ad09e9fb85
Daniel Ebuka Mabia <danielebukamabia@gmail.com>
2024-05-30 21:00:53 +0000

    removed unittest_in_js

--- 0x06-unittests_in_js/3-payment.js
+++
@@ -1,9 +0,0 @@
-const Utils = require('./utils');
-
-function sendPaymentRequestToApi(totalAmount, totalShipping) {
-	const result = Utils.calculateNumber('SUM', totalAmount, totalShipping);
-	console.log(`The total is: ${result}`);
-	return result;
-}
-
-module.exports = sendPaymentRequestToApi;
--- 0x06-unittests_in_js/6-payment_token.js
+++
@@ -1,8 +0,0 @@
-
-function getPaymentTokenFromAPI(success) {
-	if (success === true) {
-		return Promise.resolve({data: 'Successful response from the API' })
-	}
-}
-
-module.exports =  getPaymentTokenFromAPI;
