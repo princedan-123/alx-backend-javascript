6df6992f6e88b0bf4c5cbbe01be781d300a40dfb
Daniel Ebuka Mabia <danielebukamabia@gmail.com>
2024-08-19 14:45:20 +0100

    updated task 2

--- 0x05-Node_JS_basic/2-read_file.js
+++ 0x05-Node_JS_basic/2-read_file.js
@@ -1,29 +1,32 @@
 const fs = require('fs');
 
-let content = '';
-const csField = [];
-const sweField = [];
-try {
-  content = fs.readFileSync('./database.csv', 'utf8');
-  const totalStudent = content.split('\n');
-  totalStudent.shift();
-  console.log(`Number of students: ${totalStudent.length}`);
-  for (const student of totalStudent) {
-    if (student.includes('CS')) {
-      const studentRow = student.split(',');
-      const firstName = studentRow[0];
-      csField.push(firstName);
+function countStudents(path) {
+  let content = '';
+  const csField = [];
+  const sweField = [];
+  try {
+    content = fs.readFileSync(path, 'utf8');
+    const totalStudent = content.split('\n');
+    totalStudent.shift();
+    console.log(`Number of students: ${totalStudent.length}`);
+    for (const student of totalStudent) {
+      if (student.includes('CS')) {
+        const studentRow = student.split(',');
+        const firstName = studentRow[0];
+        csField.push(firstName);
+      }
+      if (student.includes('SWE')) {
+        const studentRow = student.split(',');
+        const firstName = studentRow[0];
+        sweField.push(firstName);
+      }
     }
-    if (student.includes('SWE')) {
-      const studentRow = student.split(',');
-      const firstName = studentRow[0];
-      sweField.push(firstName);
+    console.log(`Number of students in CS: ${csField.length}. List: ${csField.join(', ')}`);
+    console.log(`Number of students in SWE: ${sweField.length}. List: ${sweField.join(', ')}`);
+  } catch (err) {
+    if (err.code === 'ENOENT') {
+      throw new Error('Cannot load the database');
     }
-  }
-  console.log(`Number of students in CS: ${csField.length}. List: ${csField.join(', ')}`);
-  console.log(`Number of students in SWE: ${sweField.length}. List: ${sweField.join(', ')}`);
-} catch (err) {
-  if (err.code === 'ENOENT') {
-    console.log('Cannot load the database');
   }
 }
+module.exports = countStudents;
