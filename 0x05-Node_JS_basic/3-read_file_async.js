c6b7d488bd8cae73ba78eca8383a4858d3d436ba
Daniel Ebuka Mabia <danielebukamabia@gmail.com>
2024-08-19 16:48:42 +0100

    task 3

---
+++ 0x05-Node_JS_basic/3-read_file_async.js
@@ -0,0 +1,36 @@
+const fs = require('fs');
+
+function countStudents(path) {
+  return new Promise((resolve, reject) => {
+    fs.readFile(path, { encoding: 'utf-8' }, (err, data) => {
+      if (data) {
+        let content = '';
+        const csField = [];
+        const sweField = [];
+        content = fs.readFileSync(path, 'utf8');
+        const totalStudent = content.split('\n');
+        totalStudent.shift();
+        console.log(`Number of students: ${totalStudent.length}`);
+        for (const student of totalStudent) {
+          if (student.includes('CS')) {
+            const studentRow = student.split(',');
+            const firstName = studentRow[0];
+            csField.push(firstName);
+          }
+          if (student.includes('SWE')) {
+            const studentRow = student.split(',');
+            const firstName = studentRow[0];
+            sweField.push(firstName);
+          }
+        }
+        console.log(`Number of students in CS: ${csField.length}. List: ${csField.join(', ')}`);
+        console.log(`Number of students in SWE: ${sweField.length}. List: ${sweField.join(', ')}`);
+        resolve();
+      } else {
+        const error = new Error('Cannot load the database');
+        reject(error);
+      }
+    });
+  });
+}
+module.exports = countStudents;
