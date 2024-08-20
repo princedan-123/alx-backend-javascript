693aa4c7f613f08ef9055572c21802707cc1a3fa
Daniel Ebuka Mabia <danielebukamabia@gmail.com>
2024-08-20 06:06:17 +0100

    task 6 used send

--- 0x05-Node_JS_basic/6-http_express.js
+++ 0x05-Node_JS_basic/6-http_express.js
@@ -2,7 +2,7 @@ const express = require('express');
 
 const app = express();
 app.get('/', (req, res) => {
-  res.end('Hello Holberton School!\n');
+  res.send('Hello Holberton School!\n');
 });
 app.listen(1245);
 module.exports = app;
