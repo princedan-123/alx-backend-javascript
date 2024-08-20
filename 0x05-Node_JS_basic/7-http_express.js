dc74f0ea47c3ffd0883b1ed064d81408239de44b
Daniel Ebuka Mabia <danielebukamabia@gmail.com>
2024-08-20 06:02:20 +0100

    task 7

---
+++ 0x05-Node_JS_basic/7-http_express.js
@@ -0,0 +1,55 @@
+const express = require('express');
+
+const fs = require('fs');
+
+function countStudents(path) {
+  return new Promise((resolve, reject) => {
+    fs.readFile(path, { encoding: 'utf-8' }, (err, data) => {
+      if (data) {
+        let content = '';
+        const csField = [];
+        const sweField = [];
+        content = fs.readFileSync(path, 'utf8');
+        const totalStudent = content.split('\n');
+        totalStudent.shift();
+        const studentNumber = `Number of students: ${totalStudent.length}`;
+        for (const student of totalStudent) {
+          if (student.includes('CS')) {
+            const studentRow = student.split(',');
+            const firstName = studentRow[0];
+            csField.push(firstName);
+          }
+          if (student.includes('SWE')) {
+            const studentRow = student.split(',');
+            const firstName = studentRow[0];
+            sweField.push(firstName);
+          }
+        }
+        const csStudents = `Number of students in CS: ${csField.length}. List: ${csField.join(', ')}`;
+        const sweStudents = `Number of students in SWE: ${sweField.length}. List: ${sweField.join(', ')}`;
+        const result = `${studentNumber}\n${csStudents}\n${sweStudents}`;
+        resolve(result);
+      } else {
+        const error = new Error('Cannot load the database');
+        reject(error);
+      }
+    });
+  });
+}
+const path = process.argv[2];
+const app = express();
+app.get('/', (req, res) => {
+  res.send('Hello Holberton School!');
+});
+app.get('/students', (req, res) => {
+  const msg = 'This is the list of our students';
+  countStudents(path).then((data) => {
+    const result = `${msg}\n${data}`;
+    res.send(result);
+  })
+    .catch((error) => {
+      res.send(error.message);
+    });
+});
+app.listen(1245);
+module.exports = app;
