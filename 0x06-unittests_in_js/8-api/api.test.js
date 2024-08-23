bed769f4f8b8606660877ea33524d51607cc2ce1
Daniel Ebuka Mabia <danielebukamabia@gmail.com>
2024-08-23 19:18:35 +0100

    changed test suite description to Index page

--- 0x06-unittests_in_js/8-api/api.test.js
+++ 0x06-unittests_in_js/8-api/api.test.js
@@ -1,7 +1,7 @@
 const request = require('request');
 const expect = require('chai').expect;
 
-describe('integration test on express server', () => {
+describe('Index page', () => {
   it('testing the index page', (done) => {
     request('http://localhost:7865', (error, response, body) => {
       expect(response.statusCode).to.equal(200);
