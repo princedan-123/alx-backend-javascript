72ba165fc6b3a602ff41245fb26693649ec10058
Daniel Ebuka Mabia <danielebukamabia@gmail.com>
2024-08-24 14:49:37 +0100

    ammended test

--- 0x06-unittests_in_js/8-api/api.test.js
+++ 0x06-unittests_in_js/8-api/api.test.js
@@ -2,8 +2,9 @@ const request = require('request');
 const expect = require('chai').expect;
 
 describe('Index page', () => {
-  it('testing the index page', (done) => {
-    request('http://localhost:7865', (error, response, body) => {
+  const endpoint = 'http://localhost:7865';
+  it('Returns the right status', (done) => {
+    request(endpoint, (error, response, body) => {
       expect(response.statusCode).to.equal(200);
       expect(body).to.be.a('string');
       expect(body).to.equal('Welcome to the payment system');
