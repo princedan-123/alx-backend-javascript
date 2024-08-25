9a4cbe1bfefe9422cf8d1ba65ee4552ca37cf026
Daniel Ebuka Mabia <danielebukamabia@gmail.com>
2024-08-25 01:54:27 +0100

    added / route

--- 0x06-unittests_in_js/9-api/api.js
+++ 0x06-unittests_in_js/9-api/api.js
@@ -1,6 +1,9 @@
 const express = require('express');
 
 const app = express();
+app.get('/', (req, res) => {
+  res.send('Welcome to the payment system').status(200);
+})
 app.get('/cart/:id(\\d+)', (req, res) => {
   const { id } = req.params;
   res.send(`Payment methods for cart ${id}`)
--- 0x06-unittests_in_js/9-api/api.test.js
+++ 0x06-unittests_in_js/9-api/api.test.js
@@ -2,6 +2,14 @@ const request = require('request');
 const expect = require('chai').expect;
 
 describe('Index page', () => {
+  it('Returns the right status for /', (done) => {
+    request('http://localhost:7865', (error, response, body) => {
+      expect(response.statusCode).to.equal(200);
+      expect(body).to.be.a('string');
+      expect(body).to.equal('Welcome to the payment system');
+      done();
+    })
+  })
   it('Returns the right status code when parameter is a number', (done) => {
     request('http://localhost:7865/cart/12', (error, response, body) => {
       expect(response.statusCode).to.equal(200);
